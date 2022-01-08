259b2ed410b27254317ea49db98390f7cd9675b4
m19e <nexus.me7290@gmail.com>
2022-01-09 02:52:21 +0900

    Add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mode: "jit",
+    purge: ["./app/**/*.{ts,tsx}"],
+    darkMode: "media",
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
