d185c29241296235f78dd3726e91e8f7c10f1bf5
m19e <nexus.me7290@gmail.com>
2022-01-09 04:24:41 +0900

    Export links function for load tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,39 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "remix";
+import type { LinksFunction, MetaFunction } from "remix";
+
+import styles from "~/styles/generated.css";
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+    return { title: "New Remix App" };
+};
+
+/**
+ * The `links` export is a function that returns an array of objects that map to
+ * the attributes for an HTML `<link>` element. These will load `<link>` tags on
+ * every route in the app, but individual routes can include their own links
+ * that are automatically unloaded when a user navigates away from the route.
+ *
+ * https://remix.run/api/app#links
+ */
+export const links: LinksFunction = () => {
+    return [{ rel: "stylesheet", href: styles }];
 };
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width,initial-scale=1" />
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <Outlet />
+                <ScrollRestoration />
+                <Scripts />
+                {process.env.NODE_ENV === "development" && <LiveReload />}
+            </body>
+        </html>
+    );
 }
