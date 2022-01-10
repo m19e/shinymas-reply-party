9a9abbac3c6b1b4819cf9d52d339a10976a58175
m19e <nexus.me7290@gmail.com>
2022-01-10 22:14:29 +0900

    Require full_text prop

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,13 +1,10 @@
 import type { TweetV1 } from "twitter-api-v2";
 
-export type TrimmedTweet = Pick<
+export interface TrimmedTweet extends PickedTweetV1 {
+    full_text: string;
+}
+
+type PickedTweetV1 = Pick<
     TweetV1,
-    | "created_at"
-    | "id_str"
-    | "full_text"
-    | "in_reply_to_status_id_str"
-    | "in_reply_to_user_id_str"
-    | "in_reply_to_screen_name"
-    | "entities"
-    | "extended_entities"
+    "created_at" | "id_str" | "in_reply_to_status_id_str" | "in_reply_to_user_id_str" | "in_reply_to_screen_name" | "entities" | "extended_entities"
 >;
