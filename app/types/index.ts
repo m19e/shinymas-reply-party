2f8529dcb6c70473f8d64bd7bbc33fd98afa5b96
m19e <nexus.me7290@gmail.com>
2022-01-14 01:47:36 +0900

    Add idol name type

--- app/types/index.ts
+++ app/types/index.ts
@@ -8,3 +8,5 @@ type PickedTweetV1 = Pick<
     TweetV1,
     "created_at" | "id_str" | "in_reply_to_status_id_str" | "in_reply_to_user_id_str" | "in_reply_to_screen_name" | "entities" | "extended_entities"
 >;
+
+export type IdolName = "all" | "meguru" | "juri" | "chiyuki" | "asahi" | "madoka" | "hinana";
