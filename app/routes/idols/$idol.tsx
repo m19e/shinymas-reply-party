1aab1bdf95724b0c4582105aa85738289707f467
m19e <nexus.me7290@gmail.com>
2022-01-21 02:29:38 +0900

    Set empty array to initial tweets

--- app/routes/idols/$idol.tsx
+++ app/routes/idols/$idol.tsx
@@ -23,7 +23,7 @@ const Idol = () => {
     const params = useParams();
     const navigate = useNavigate();
 
-    const [tweets, setTweets] = useState(typedTweets);
+    const [tweets, setTweets] = useState<typeof typedTweets>([]);
 
     useEffect(() => {
         if (data.idol === "all") {
@@ -37,9 +37,9 @@ const Idol = () => {
     }, [data.idol, params.idol]);
 
     return (
-        <div style={{ minWidth: "400px" }}>
+        <div className="min-h-screen flex flex-col" style={{ minWidth: "400px" }}>
             <Header />
-            <div className="bg-pattern flex flex-col items-center border-t-2 border-slate-400 py-4">
+            <div className="bg-pattern flex-1 flex flex-col items-center border-t-2 border-slate-400 py-4">
                 <div className="w-5/6 max-w-xl md:max-w-2xl 2xl:max-w-3xl flex flex-col items-center">
                     <div className="w-full inline-flex justify-start md:justify-center gap-2 md:gap-3 flex-wrap md:flex-nowrap mb-4">
                         {idols.map((idol) => (
