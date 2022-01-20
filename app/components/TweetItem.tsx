148491272065eb1f8a0b4275cf0afa9b1774a86a
m19e <nexus.me7290@gmail.com>
2022-01-21 03:07:08 +0900

    Extract idol-label component

--- app/components/TweetItem.tsx
+++ app/components/TweetItem.tsx
@@ -72,20 +72,26 @@ const DecoratedText = ({ tweet }: Props) => {
     return <p className="block whitespace-pre-wrap" dangerouslySetInnerHTML={{ __html: createDecoratedHtml(tweet) }}></p>;
 };
 
+const IdolLabel: FC<{ idol: TrimmedTweet["idol"] }> = ({ idol }) => {
+    return (
+        <div className="w-28 h-6 ml-0.5 mt-0.5 rounded-tl-md overflow-hidden relative">
+            <div className={idol + " w-full h-6 rounded-md absolute -left-1 -top-1 -skew-x-12"}>
+                <div className="skew-x-12">
+                    <div className="flex justify-center pl-1 pt-0.5">
+                        <span className="noto-sans-jp font-black text-sm text-white tracking-wider">{idol.toUpperCase()}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
 export const TweetItem: FC<Props> = ({ tweet }) => {
     const time = getDisplayTime(tweet.created_at);
 
     return (
         <div className="flex flex-col bg-white rounded-lg shadow-md border border-gray-500">
-            <div className="w-28 h-6 ml-0.5 mt-0.5 rounded-tl-md overflow-hidden relative">
-                <div className={tweet.idol + " w-full h-6 rounded-md absolute -left-1 -top-1 -skew-x-12"}>
-                    <div className="skew-x-12">
-                        <div className="flex justify-center pl-1 pt-0.5">
-                            <span className="noto-sans-jp font-black text-sm text-white tracking-wider">{tweet.idol.toUpperCase()}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <IdolLabel idol={tweet.idol} />
             <div className="px-4">
                 <DecoratedText tweet={tweet} />
             </div>
