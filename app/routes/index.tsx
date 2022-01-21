35ef11853cafd49b2e1956c16a2e5f704491c4e7
m19e <nexus.me7290@gmail.com>
2022-01-21 23:30:57 +0900

    Set index page meta func

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,25 @@
+import type { MetaFunction } from "remix";
+import CardImage from "~/statics/images/icon-250x250.png";
+
 import { typedTweets, navs } from "../consts";
 import { Header } from "../components/Header";
 import { IdolLink } from "../components/IdolLink";
 import { TweetList } from "../components/TweetList";
+
+export const meta: MetaFunction = () => {
+    return {
+        title: "#シャニマスリフ゜ハ゜",
+        description: "#シャニマスリフ゜ハ゜ #２８３をひろげよう",
+        "og:site_name": "#シャニマスリフ゜ハ゜",
+        "og:title": "#シャニマスリフ゜ハ゜",
+        "og:description": "#シャニマスリフ゜ハ゜ #２８３をひろげよう",
+        "og:image": CardImage,
+        "twitter:image": CardImage,
+        "twitter:card": "summary",
+        "twitter:title": "#シャニマスリフ゜ハ゜",
+        "twitter:description": "#シャニマスリフ゜ハ゜ #２８３をひろげよう",
+    };
+};
 
 const Root = () => {
     return (
