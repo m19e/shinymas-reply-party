e2dbe4dfc7f3cfb564dde571760e3dd944e80bb5
m19e <nexus.me7290@gmail.com>
2022-01-22 21:11:55 +0900

    Back

--- app/components/TweetIdolLabel.tsx
+++ app/components/TweetIdolLabel.tsx
@@ -2,8 +2,8 @@ import type { FC } from "react";
 
 export const TweetIdolLabel: FC<{ idol: string }> = ({ idol }) => {
     return (
-        <div className="w-28 h-6 ml-0.5 mt-0.5 rounded-tl-md overflow-hidden relative">
-            <div className={idol + " w-full h-6 rounded-md absolute -left-1 -top-0.5 -skew-x-12"}>
+        <div className="w-28 h-7 ml-0.5 mt-0.5 rounded-tl-md overflow-hidden relative">
+            <div className={idol + " w-full h-6 rounded-md absolute -left-1 -top-1 -skew-x-12"}>
                 <div className="skew-x-12">
                     <div className="flex justify-center pl-1 pt-0.5">
                         <span className="noto-sans-jp font-black text-sm text-white tracking-wider">{idol.toUpperCase()}</span>
