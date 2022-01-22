9f0adb7153674a54f9c7d43701d0518a566d9702
m19e <nexus.me7290@gmail.com>
2022-01-22 20:52:43 +0900

    Fix logo, header size

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,6 +11,17 @@ const Logo = () => {
     );
 };
 
+const LogoSmall = () => {
+    return (
+        <h1 className="zen-maru-gothic text-4xl text-transparent bg-clip-text bg-gradient-to-b from-pink-500 to-yellow-400 py-0.5">
+            <span className="tracking-tighter">#</span>
+            <span className="pr-4" style={{ letterSpacing: -8 }}>
+                <span style={{ letterSpacing: -12 }}>シ</span>ャニマ<span style={{ letterSpacing: -13 }}>スリ</span>プパ
+            </span>
+        </h1>
+    );
+};
+
 const Share = () => {
     return (
         <a
@@ -38,10 +49,10 @@ const Share = () => {
 
 export const Header = () => {
     return (
-        <div className="w-full inline-flex justify-center items-center h-20 bg-white border-b-2 mb-1 px-4">
+        <div className="w-full inline-flex justify-center items-center h-16 bg-white border-b-2 mb-1 px-4">
             <div className="w-full max-w-4xl inline-flex justify-between items-center">
                 <Link to="/">
-                    <Logo />
+                    <LogoSmall />
                 </Link>
                 <Share />
             </div>
